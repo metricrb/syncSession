b5efe598888cca07cbc3877da5a99c406798d3dc
michaelrbx <startreksrbx@gmail.com>
2023-06-29 22:53:32 +0100

    Added Training Type to Cmdr

--- system/Server/Cmdr/BuiltInTypes/Trainings.lua
+++ system/Server/Cmdr/BuiltInTypes/Trainings.lua
@@ -1,12 +1,12 @@
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local BindableFunction = ReplicatedStorage:WaitForChild('GetLiveSessions')
 return function(p1)
-	local cards = BindableFunction:Invoke('Shifts')
+	local cards = BindableFunction:Invoke('Trainings')
 	local tbl = {}
 	for i, v in pairs(cards) do
 		table.insert(tbl, v)
 	end;
-	p1:RegisterType("shifts", p1.Cmdr.Util.MakeEnumType("Shifts", tbl));
+	p1:RegisterType("trainings", p1.Cmdr.Util.MakeEnumType("Trainings", tbl));
 end;
 
 
