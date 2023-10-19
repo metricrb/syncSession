d3aacc07936a0a7be665b7e31566d8997005cbc6
michaelrbx <startreksrbx@gmail.com>
2023-10-19 23:39:51 +0100

    test

--- system/Server/CmdrSetup.server.lua
+++
@@ -1,5 +0,0 @@
-local Server = script.Parent
-local Cmdr = require(Server:WaitForChild('Cmdr'))
-
--- Cmdr:RegisterDefaultCommands() -- This loads the default set of commands that Cmdr comes with. (Optional)
-Cmdr:RegisterCommandsIn(Cmdr.CmdrCommands) 
